0ceae070dc15a6812c6af27b40c18437a59f1ff9
David McCabe <dave.mccabe@idealist.org>
2014-12-05 15:14:45 -0800

    Jest set up.

---
+++ examples/simple/__tests__/checklist-test.js
@@ -0,0 +1,26 @@
+/** @jsx React.DOM */
+
+jest.dontMock('../checklist.js');
+describe('CheckboxWithLabel', function() {
+  it('changes the text after click', function() {
+    var React = require('react/addons');
+    var CheckboxWithLabel = require('../checklist.js');
+    var TestUtils = React.addons.TestUtils;
+
+    // Render a checkbox with label in the document
+    var checkbox = TestUtils.renderIntoDocument(
+      <CheckboxWithLabel labelOn="On" labelOff="Off" />
+    );
+
+    // Verify that it's Off by default
+    var label = TestUtils.findRenderedDOMComponentWithTag(
+      checkbox, 'label');
+    expect(label.getDOMNode().textContent).toEqual('Off');
+
+    // Simulate a click and verify that it is now On
+    var input = TestUtils.findRenderedDOMComponentWithTag(
+      checkbox, 'input');
+    TestUtils.Simulate.change(input);
+    expect(label.getDOMNode().textContent).toEqual('On');
+  });
+});
